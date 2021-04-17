1c08f713a0b5b5a9aa13b77623921233af3848db
Jordan Gustafson <jordangustafson@pm.me>
2021-04-17 09:18:41 -0500

    Initial Commit

---
+++ Shared/InterruptibleAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterruptibleAnimationsApp.swift
+//  Shared
+//
+//  Created by Jordan Gustafson on 4/17/21.
+//
+
+import SwiftUI
+
+@main
+struct InterruptibleAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
